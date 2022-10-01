0183ff07168b4250cebcb4b9d79f2639765d33e3
Adjetey_Ken <kenadjei20@gmail.com>
2022-10-01 20:34:51 +0000

    inputing loerm text

---
+++ src/pages/About.js
@@ -0,0 +1,47 @@
+import React from "react";
+import { Text, View, StyleSheet } from "react-native";
+
+export default function About() {
+  return (
+    <View style={styles.container}>
+      <View style={styles.Text}>
+        <Text styles={styles.abt_txt}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime
+          mollitia, molestiae quas vel sint commodi repudiandae consequuntur
+          voluptatum laborum numquam blanditiis harum quisquam eius sed odit
+          fugiat iusto fuga praesentium optio, eaque rerum! Provident similique
+          accusantium nemo autem. Veritatis obcaecati tenetur iure eius earum ut
+          molestias architecto voluptate aliquam nihil, eveniet aliquid culpa
+          officia aut! Impedit sit sunt quaerat, odit, tenetur error, harum
+          nesciunt ipsum debitis quas aliquid. Reprehenderit, quia. Quo neque
+          error repudiandae fuga? Ipsa laudantium molestias eos sapiente
+          officiis modi at sunt excepturi expedita sint? Sed quibusdam
+          recusandae alias error harum maxime adipisci amet laborum.
+          Perspiciatis minima nesciunt dolorem! Officiis iure rerum voluptates a
+          cumque velit quibusdam sed amet tempora. Sit laborum ab, eius fugit
+          doloribus tenetur fugiat, temporibus enim commodi iusto libero magni
+          deleniti quod quam consequuntur! Commodi minima excepturi repudiandae
+          velit hic maxime doloremque. Quaerat provident commodi consectetur
+          veniam similique ad earum omnis ipsum saepe, voluptas, hic voluptates
+          pariatur est explicabo fugiat, dolorum eligendi quam cupiditate
+          excepturi mollitia maiores labore suscipit quas? Nulla, placeat.
+          Voluptatem quaerat non architecto ab laudantium modi minima sunt esse
+          temporibus sint culpa, recusandae aliquam numquam totam ratione
+          voluptas quod exercitationem fuga. Possimus quis earum veniam quasi
+          aliquam eligendi, placeat qui corporis!
+        </Text>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+  },
+  Text: {
+    marginHorizontal: 15,
+    marginVertical: 20,
+  },
+});
